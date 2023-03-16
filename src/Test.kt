7d07930f9d42eb0aba78cb538f78b2d409103643
Praveen Singh Solanki <113306344+praveen-singh-solanki@users.noreply.github.com>
2023-03-15 21:24:12 -0400

    Update Test.kt

--- src/Test.kt
+++ src/Test.kt
@@ -6,16 +6,23 @@ open class MyParentClass {
 // Subclass
 class MyChildClass: MyParentClass() {
   fun myFunction() {
-	var count = 0
-	Thread.sleep(4000)
-	while(count < 5)
-	{
-    println(x) // x is defined in the superclass
-    try { throw Exception("Hi There!") } catch (e: Exception) { println(e) }
-	Thread.sleep(15000)
-	count++
-  	}
-	}
+    var count = 0
+    Thread.sleep(4000)
+    while(count < 5)
+    {
+      println(x) // x is defined in the superclass
+      try
+      { 
+        throw Exception("Hi There!") 
+      }
+      catch (e: Exception)
+      {
+        println(e)
+      }
+      Thread.sleep(1000)
+      count++
+    }
+  }
 }
 
 // Create an object of the MyChildClass and call myFunction
